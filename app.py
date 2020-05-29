39d811df91ecf0cfc5b5027a88ad8c130ee0802c
夭暝 <@DESKTOP-01RB0UP>
2020-05-29 11:50:01 +0800

    test 1

--- app.py
+++ app.py
@@ -2,37 +2,106 @@ from flask import *
 import opSql
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'px1314'
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
+    print(session)
+    if session.get('username') is None:
+        return render_template('index.html', newurl='/login', name='', option="登录/注册")
+    else:
+        if session['priority'] == 0:
+            return render_template('index.html', newurl='/logout', name=session.get('username'), option="退出")
+        else:
+            return render_template('index.html', newurl='/logout', name=session.get('username'), option="管理")
 
 
 @app.route('/news/')
 def news():
-    return render_template('news.html')
+    if session.get('username') is None:
+        return render_template('news.html', newurl='/login', name='', option="登录/注册")
+    else:
+        return render_template('news.html', newurl='/logout', name=session.get('username'), option="退出")
 
 
-@app.route('/team')
-def team():
-    return render_template('team.html')
+@app.route('/athlete')
+def athlete():
+    if session.get('username') is None:
+        return render_template('athlete.html', newurl='/login', name='', option="登录/注册")
+    else:
+        return render_template('athlete.html', newurl='/logout', name=session.get('username'), option="退出")
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    if session.get('username') is None:
+        return render_template('about.html', newurl='/login', name='', option="登录/注册")
+    else:
+        print(session.get('username'))
+        return render_template('about.html', newurl='/logout', name=session.get('username'), option="退出")
 
 
 @app.route('/matches')
 def matches():
-    return render_template('matches.html')
+    if session.get('username') is None:
+        return render_template('matches.html', newurl='/login', name='', option="登录/注册")
+    else:
+        return render_template('matches.html', newurl='/logout', name=session.get('username'), option="退出")
 
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        # print(opSql.login(request.form['name'], request.form['pwd']))
+        name = request.form['name']
+        pwd = request.form['pwd']
+        """
+        if name in online:
+            print("logined")
+            return redirect(url_for('index'))
+        
+        """
+
+        (f, id) = opSql.login(name, pwd)
+        # print(f)
+        # session['user_id'] = id
+        if f == -1:
+            print("error")
+            return redirect('login')
+        else:
+            session['username'] = name
+            if f == 0:
+                session['id'] = id
+                session['priority'] = 0
+                return redirect(url_for('index'))
+            elif f == 1:
+                session['id'] = id
+                session['priority'] = 1
+                return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    """
+    session.pop('username')
+    session.pop('priority')
+    """
+    if session.get('username') in online:
+        del online[session.get('username')]
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        print(request.form)
+        return redirect(url_for('index'))
 
 
 @app.route('/news/<newsid>')
--- opSql.py
+++ opSql.py
@@ -1,19 +1,61 @@
 import pymysql
+import sys
+from datetime import datetime
 
 db = pymysql.connect("49.235.51.187", "root", "123456Web%", "ympy")
 cursor = db.cursor()
+fp = open('static/images/2019shijiebei.jpg', encoding='UTF-8', errors='ignore')
+pic = fp.read()
+fp.close()
+'''
+try:
+    sq = "INSERT INTO img VALUE ('%s',%d,'%s')" % (2020, 1, pymysql.escape_string(pic))
+    cursor.execute(sq)
+    db.commit()
+    print("done")
+except pymysql:
+    print("error")
+'''
 
 
-def login(id, pwd):
-    sql = "SELECT * FROM user WHERE id = %d" % id
+try:
+    sq = "SELECT img FROM img WHERE newsid = '%s'" % 2020
+    cursor.execute(sq)
+    img = cursor.fetchone()[0]
+    fp = open('static/images/test.jpg', 'wb')
+    fp.write(img)
+    fp.close()
+except pymysql:
+    print("error")
+
+
+def login(name, pwd):
+    sql = "SELECT * FROM user WHERE name = '%s'" % name
     try:
         cursor.execute(sql)
         result = cursor.fetchall()
-        print(type(result[0][1]))
-        if str(pwd) == result[0][1]:
-            return True
+        if len(result) != 0:
+            if str(pwd) == result[0][2]:  # 比较密码是否正确
+                if result[0][4] == 1:  # 是否为管理员（1为管理员）
+                    return 1, result[0][0]
+                else:
+                    return 0, result[0][0]
         else:
-            return False
+            return -1, 0
+    except pymysql:
+        return -1, 0
+
+
+def register(name, email, pwd=123456):
+    sql = "SELECT * FROM user ORDER BY id DESC"
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        id = result[0][0] + 1
+        sql = "INSERT INTO user VALUES (%d,'%s','%s','%s',0)" % (id, name, pwd, email)
+        cursor.execute(sql)
+        db.commit()
+        return "success"
     except pymysql:
         return "error"
 
@@ -28,4 +70,17 @@ def get_news(id):
         return "error"
 
 
+def insert_news():
+    sql = "SELECT NOW()"
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+    except pymysql:
+        return "error"
 
+
+'''
+s = str(datetime.strftime(insert_news()[0][0], '%Y%m%d%H%M')) + '02' + str(101)
+print(s)
+'''
