d7cd4d1409553879faa53a0e18321837353da7b4
夭暝 <@DESKTOP-01RB0UP>
2020-05-31 21:53:04 +0800

    release 1

--- app.py
+++ app.py
@@ -7,22 +7,23 @@ app.config['SECRET_KEY'] = 'px1314'
 
 @app.route('/')
 def index():
-    print(session)
+    # print(session)
     if session.get('username') is None:
         return render_template('index.html', newurl='/login', name='', option="登录/注册")
     else:
-        if session['priority'] == 0:
-            return render_template('index.html', newurl='/logout', name=session.get('username'), option="退出")
-        else:
-            return render_template('index.html', newurl='/logout', name=session.get('username'), option="管理")
+        return render_template('index.html', name=session.get('username'), priority=session.get('priority'))
 
 
 @app.route('/news/')
 def news():
+    news = opSql.get_all_news()
+    # imgs = opSql.get_cover_imgs()
+    # print(news)
+    # print(imgs)
     if session.get('username') is None:
-        return render_template('news.html', newurl='/login', name='', option="登录/注册")
+        return render_template('news.html', newurl='/login', name='', option="登录/注册", resp=news)
     else:
-        return render_template('news.html', newurl='/logout', name=session.get('username'), option="退出")
+        return render_template('news.html', name=session.get('username'), priority=session.get('priority'), resp=news)
 
 
 @app.route('/athlete')
@@ -30,7 +31,7 @@ def athlete():
     if session.get('username') is None:
         return render_template('athlete.html', newurl='/login', name='', option="登录/注册")
     else:
-        return render_template('athlete.html', newurl='/logout', name=session.get('username'), option="退出")
+        return render_template('athlete.html', name=session.get('username'), priority=session.get('priority'))
 
 
 @app.route('/about')
@@ -38,8 +39,8 @@ def about():
     if session.get('username') is None:
         return render_template('about.html', newurl='/login', name='', option="登录/注册")
     else:
-        print(session.get('username'))
-        return render_template('about.html', newurl='/logout', name=session.get('username'), option="退出")
+        # print(session.get('username'))
+        return render_template('about.html', name=session.get('username'), priority=session.get('priority'))
 
 
 @app.route('/matches')
@@ -89,8 +90,6 @@ def logout():
     session.pop('username')
     session.pop('priority')
     """
-    if session.get('username') in online:
-        del online[session.get('username')]
     session.clear()
     return redirect(url_for('index'))
 
@@ -100,14 +99,48 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        print(request.form)
-        return redirect(url_for('index'))
+        # print(request.form)
+        result = opSql.register(request.form['name'], request.form['email'], request.form['pwd'])
+        if result == 'success':
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for(register))
 
 
 @app.route('/news/<newsid>')
 def getnews(newsid):
-    result = opSql.get_news(newsid)
-    return render_template('newscontent.html', title=result[0][1], content1=result[0][1], content2=result[0][2])
+    content = opSql.get_new(newsid)
+    imgs = opSql.get_new_img(newsid)
+    if session.get('username') is None:
+        return render_template('newscontent.html', title=content[1], subtitle=content[2], content=content[3], imgs=imgs)
+    else:
+        return render_template('newscontent.html', title=content[1], subtitle=content[2], content=content[3], imgs=imgs,
+                               name=session.get('username'), priority=session.get('priority'))
+
+
+@app.route('/control', methods=['POST', 'GET'])
+def control():
+    if request.method == 'GET':
+        return render_template('control.html')
+    else:
+        img = request.files['file']
+        dirname = opSql.insert_news(request.form, session.get('id'))
+        path = "static/images/news_img/" + dirname + "/1.jpg"
+        img.save(path)
+        return redirect(url_for('index'))
+
+
+@app.route('/delete/', methods=['POST', 'GET'])
+def delete():
+    news = opSql.get_all_news()
+    return render_template('delete.html', resp=news)
+
+
+@app.route('/delete/<newsid>', methods=['POST', 'GET'])
+def delete_new(newsid):
+    opSql.delete_new(newsid)
+    # print(newsid)
+    return redirect(url_for('delete'))
 
 
 if __name__ == '__main__':
--- opSql.py
+++ opSql.py
@@ -1,32 +1,10 @@
 import pymysql
-import sys
+import os
+import shutil
 from datetime import datetime
 
 db = pymysql.connect("49.235.51.187", "root", "123456Web%", "ympy")
 cursor = db.cursor()
-fp = open('static/images/2019shijiebei.jpg', encoding='UTF-8', errors='ignore')
-pic = fp.read()
-fp.close()
-'''
-try:
-    sq = "INSERT INTO img VALUE ('%s',%d,'%s')" % (2020, 1, pymysql.escape_string(pic))
-    cursor.execute(sq)
-    db.commit()
-    print("done")
-except pymysql:
-    print("error")
-'''
-
-
-try:
-    sq = "SELECT img FROM img WHERE newsid = '%s'" % 2020
-    cursor.execute(sq)
-    img = cursor.fetchone()[0]
-    fp = open('static/images/test.jpg', 'wb')
-    fp.write(img)
-    fp.close()
-except pymysql:
-    print("error")
 
 
 def login(name, pwd):
@@ -60,27 +38,89 @@ def register(name, email, pwd=123456):
         return "error"
 
 
-def get_news(id):
-    sql = "SELECT * FROM newsinfo WHERE id = %s" % id
+def get_new(id):
+    sql = "SELECT * FROM newsinfo WHERE id = '%s'" % id
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result[0]
+    except pymysql:
+        print("get_new() error")
+
+
+def get_new_img(id):
+    sql = "SELECT img FROM img WHERE newsid = %s ORDER BY imgid ASC " % id
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result[0]
+    except pymysql:
+        print("get_new_img error")
+
+
+def get_cover_imgs():
+    sql = "SELECT img FROM img WHERE imgid = 1 ORDER BY newsid DESC "
     try:
         cursor.execute(sql)
         result = cursor.fetchall()
         return result
     except pymysql:
-        return "error"
+        print("get_cover_imgs() error")
 
 
-def insert_news():
+def get_all_news():
+    sql = "SELECT newsinfo.*,img,user.name FROM newsinfo,img,user WHERE newsinfo.id=img.newsid and " \
+          "newsinfo.userid=user.id ORDER BY newsinfo.id DESC "
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+    except pymysql:
+        print("get_all_news() error")
+
+
+def insert_news(info, userid):
+    path = 'static/images/news_img/'
     sql = "SELECT NOW()"
     try:
         cursor.execute(sql)
-        result = cursor.fetchall()
-        return result
+        time = cursor.fetchall()
+        newsid = str(datetime.strftime(time[0][0], '%Y%m%d%H%M')) + info['cateid'] + str(userid)
+        sql = "INSERT INTO newsinfo VALUES ('%s','%s','%s','%s',%d,'%s','%s')" % (newsid, info['title'], info['subtitle'], info['content'], userid, time[0][0], info['cateid'])
+        os.mkdir(path + str(newsid))
+        cursor.execute(sql)
+        sql = "INSERT INTO img VALUES ('%s',%d,'%s')" % (newsid, 1, str(newsid) + '/1.jpg')
+        cursor.execute(sql)
+        db.commit()
+        return newsid
     except pymysql:
-        return "error"
+        db.rollback()
+        return print("insert_news() error")
 
+
+def delete_new(id):
+    path = 'static/images/news_img/'
+    sql = "DELETE FROM newsinfo WHERE id = '%s'" % id
+    try:
+        cursor.execute(sql)
+        sql = "DELETE FROM img WHERE newsid = '%s'" %id
+        cursor.execute(sql)
+        db.commit()
+        shutil.rmtree(path + str(id))
+    except pymysql:
+        db.rollback()
+        print("delete_new() error")
 
 '''
 s = str(datetime.strftime(insert_news()[0][0], '%Y%m%d%H%M')) + '02' + str(101)
 print(s)
+
+imgs = get_cover_imgs()
+for item in (news, imgs):
+    print(item[0]+item[1])
+news = get_all_news()
+print(len(news))
+re = get_new_img(20191110132902101)
+print(re[0])
 '''
+
